4ae467634c85d8165b168e25447f81564cb951ad
Ibrahim Shah <104ibrahimshah@gmail.com>
2021-01-17 14:54:18 -0600

    Script.JS created, should load model

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("model is slolasda");
+async function loadModel(){
+
+
+  model = undefined;
+  model = await tf.loadLayersModel("https://github.com/ArjSan1/covid-classifier/blob/main/models/model.json");
+
+  console.log("Model is loaded");
+
+
+
+
+
+}
+loadModel();
+
